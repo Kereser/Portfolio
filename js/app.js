93f0add0563aa8444ec7e2ed8ffcfe4263311eb6
Kereser <Urregoarr00@gmail.com>
2022-04-13 20:16:05 -0500

    1.0.0 showcase-area

--- js/app.js
+++ js/app.js
@@ -7,3 +7,13 @@ function stickNavbar() {
 // stickNavbar(); //? Revisar si da errores para agregarlo
 
 window.addEventListener('scroll', stickNavbar);
+
+const sr = ScrollReveal({
+  duration: 2500,
+  distance: '60px',
+});
+
+sr.reveal('.showcase-info', { delay: 500 });
+sr.reveal('.showcase-img', { origin: 'top', delay: 500 });
+
+// <a href="https://iconscout.com/illustrations/software-engineer" target="_blank">Software engineer Illustration</a> by <a href="https://iconscout.com/contributors/delesign" target="_blank">Delesign Graphics</a>
